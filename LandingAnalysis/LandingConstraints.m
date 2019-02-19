0b006947520d9ee78031ddabc72d046477017dc2
Trexter <sheridak@purdue.edu>
2019-02-19 16:05:09 -0500

    cleaning and making easier to use

---
+++ LandingAnalysis/LandingConstraints.m
@@ -0,0 +1,36 @@
+classdef LandingConstraints
+    %LANDINGCONSTRAINTS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        payloadMass; % 1x1 (kg)
+        propellantMass; % 1x1 (kg)
+        vehicleHeight; % 1x1 (m) height of vehicle
+        vehicleRadius; % 1x1 (m) radius of cylindrical vehicle
+        
+        % Trajectory Generator Settings
+        g; % m/s^2
+        maxForcePerMotor; % N
+        minForcePerMotor; % N
+        
+        initialMass; %kg
+        Isp;
+        inertiaTensor; % 3x3 inertia tensor
+        
+        thrusterLeverArm = 2; % meters (the distant from the com to each thruster)
+        thrusterCount = 8;
+        thrusterMap; % used to map moments and total body z force to individual thruster outputs.
+        
+        
+        trajectoryConstraints; % [[pos1;vel1], [pos2;vel2], [pos3;vel3], ...]
+                               % each constraint is in the frame centered
+                               % at the landing site.
+        
+        
+    end
+    
+    methods
+        
+    end
+end
+
---
+++ LandingAnalysis/computeLandingTrajectory.m
@@ -0,0 +1,17 @@
+function [traj, flightTime, segmentLogs] = computeLandingTrajectory(landingConstraints)
+%COMPUTETHRUSTERREQUIREMENTS Will compute a trajectory through the
+%waypoints covered in my (Kevin Sheridan) 3rd presentation.
+
+% generate the trajectory and plot.
+
+start = [landingConstraints.trajectoryConstraints(1:3, 1), landingConstraints.trajectoryConstraints(4:6, 1), zeros(3, 1), zeros(3, 1), zeros(3, 1)];
+mid = landingConstraints.trajectoryConstraints(1:6, 2:(end-1));
+final = [landingConstraints.trajectoryConstraints(1:3, end), landingConstraints.trajectoryConstraints(4:6, end), zeros(3, 1), zeros(3, 1), zeros(3, 1)];
+
+warning('off')
+
+[traj, flightTime, segmentLogs] = minimumTimeTrajectoryGenerator(start, mid, final, 'VEL', landingConstraints, 40);
+
+fprintf('\nTotal Flight Time: %f s \nFinal Mass: %f kg\n', flightTime, segmentLogs{end}.mass(end));
+
+end
